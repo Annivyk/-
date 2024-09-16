2809b91e90e79dde9d72794810d965bd96afe05a
anwyn <2155341466@qq.com>
2024-09-17 03:37:25 +0800

    完成了在文件中直接引入add.c，开始进行引入.h的探究

---
+++ Preparative_01/For_add.c
@@ -0,0 +1,8 @@
+
+#include "add.c"  // 直接包含 add.c
+
+int main() {
+    int result = add(2, 3);
+    std::cout << "Result: " << result << std::endl;
+    return 0;
+}
---
+++ Preparative_01/For_h.c
@@ -0,0 +1,8 @@
+
+#include "add.h"  // 引入 add.h 而不是 add.c
+
+int main() {
+    int result = add(2, 3);
+    std::cout << "Result: " << result << std::endl;
+    return 0;
+}
